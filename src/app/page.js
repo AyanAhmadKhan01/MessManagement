6e21c5d87f8526e91922788f068ad636f77c65fc
ayanahmadkhan <ayanahmadkhan042@gmail.com>
2025-06-24 23:59:24 +0530

    fourth push

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+   <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Button</Button>
+      <h1>hi</h1>
+      <h1>hi</h1>
+       <h1>hi</h1>
+    </div>
+  );
+}
